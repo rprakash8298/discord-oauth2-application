6f40feff0b6026e9772f3edd2f94cdbcfd09ec77
ravi prakash <rprakash8298@gmail.com>
2021-04-17 00:22:48 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Switch,Redirect } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import PrivateRoute from './components/PrivateRoute'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 
 function App() {
-  const [token, setToken] = useState(false)
-  if (localStorage.getItem('token')) {
-     setToken(true)
-   }
+ 
   return (
   <>
     <Router>
       <Switch>
-    {token ? <Redirect to='/dashboard' /> : ''}
+    
         <Route exact path="/" component={Login} />
         <PrivateRoute  path='/dashboard' component={Dashboard} />
         </Switch>
